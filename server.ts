dab2e4eea27c65457cf9b42ab297e0cf18c36df1
Anson Chung <ansonchung3@gmail.com>
2025-12-06 09:51:16 +0000

    Move CORS control to backend by adding CORS header. Install new package.

--- server.ts
+++ server.ts
@@ -1,7 +1,18 @@
 import express from 'express';
 import supabase from './config/supabaseClient.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    }),
+);
+
 app.use(express.json());
 
 app.get('/api', (_req, res) => {
