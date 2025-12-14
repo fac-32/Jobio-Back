8f45c5482489d226f70c2014950eef0843a9f4d7
Anson Chung <ansonchung3@gmail.com>
2025-12-14 17:01:57 +0000

    Allow any origin for testing

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ const app = express();
 // CORS configuration - allow requests from frontend server
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
